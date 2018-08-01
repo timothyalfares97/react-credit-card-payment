fb9cea2858a3f973746acc888ebe665c4945ebf9
timothyalfares <timothy_alfares@yahoo.com>
2018-08-02 00:05:21 +1000

    Add card content, remove boilerplate template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+
 import Card from './components/Card'
 import './App.css'
 
@@ -7,13 +7,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <div className="App-card-container">
           <Card/>
         </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import './Card.css'
+import logo from '../assets/mastercard.png'
+import chip from '../assets/chip.png'
 
 class Card extends Component {
   render() {
     return (
       <div className="Card-container">
-        <span>test</span>
+        <div className="Card-bank-name">Bank Name</div>
+        <img src={chip} className="Card-chip"/>
+        <div className="Card-bank-number">1234 5678 9876 5432</div>
+        <div className="Card-bottom-row">
+          <div className="Card-name-holder">Benjamin Johnston</div>
+          <img src={logo} className="Card-logo"/>
+        </div>
       </div>
     )
   }
