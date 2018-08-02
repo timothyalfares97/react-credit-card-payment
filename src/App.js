d10d8f68767c4119b74648ef13416780f3561f3f
timothyalfares <timothy_alfares@yahoo.com>
2018-08-02 10:52:02 +1000

    Fix card layout, add theme for card

--- src/App.js
+++ src/App.js
@@ -7,8 +7,40 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1 className="App-title">Card Galleries</h1>
         <div className="App-card-container">
-          <Card/>
+          <div className="App-card-row">
+            <Card
+              bankName='ANZ Bank'
+              cardHolder='Andrew White'
+              cardNumber='1234 5678 9876 5432'
+              issuer='mastercard'
+              theme='light'
+            />
+            <Card
+              bankName='ANZ Bank'
+              cardHolder='Andrew White'
+              cardNumber='1234 5678 9876 5432'
+              issuer='mastercard'
+              theme='dark'
+            />
+          </div>
+          <div className="App-card-row">
+            <Card
+              bankName='ME Bank'
+              cardHolder='Benny Doe'
+              cardNumber='4321 1234 5555 5454'
+              issuer='visa'
+              theme='light'
+            />
+            <Card
+              bankName='ME Bank'
+              cardHolder='Benny Doe'
+              cardNumber='4321 1234 5555 5454'
+              issuer='visa'
+              theme='dark'
+            />
+          </div>
         </div>
       </div>
     );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,89 @@
 import React, { Component } from 'react'
 import './Card.css'
-import logo from '../assets/mastercard.png'
+import backgroundLight from '../assets/backgroundLight.jpeg'
+import backgroundDark from '../assets/backgroundDark.jpeg'
+import mastercardLight from '../assets/mastercardLight.png'
+import mastercardDark from '../assets/mastercardDark.png'
+import visaDark from '../assets/visaDark.png'
+import visaLight from '../assets/visaLight.png'
 import chip from '../assets/chip.png'
 
-class Card extends Component {
+export default class Card extends Component {
+
+  static defaultProps = {
+    bankName: 'Bank Name',
+    cardHolder: 'Andrew Smith',
+    cardNumber: '**** **** **** ****',
+    issuer: '',
+    theme: 'light',
+  }
+
+  currentTheme = () => {
+    const { theme } = this.props
+
+    let currentTheme = null
+
+    switch(theme) {
+      case 'light':
+        currentTheme = {
+          color: 'black',
+          background: `url(${backgroundLight})`,
+        }
+        break
+      case 'dark':
+        currentTheme = {
+          color: 'white',
+          background: `url(${backgroundDark})`,
+        }
+        break
+    }
+
+    return currentTheme
+  }
+
+  renderIssuer = () => {
+    const { issuer, theme } = this.props
+    
+    if (issuer === '') {
+      return
+    }
+
+    let logo = null
+    let style = null
+
+    switch(issuer) {
+      case 'mastercard':
+        logo = theme === 'dark' ? mastercardDark : mastercardLight
+        style = 'Card-mastercard-logo'
+        break
+      case 'visa':
+        logo = theme === 'dark' ? visaDark : visaLight
+        style = 'Card-visa-logo'
+        break
+    }
+
+    return <img src={logo} className={style}/>
+  }
+
   render() {
+    const { bankName, cardHolder, cardNumber } = this.props
+    const currentTheme = this.currentTheme()
     return (
-      <div className="Card-container">
-        <div className="Card-bank-name">Bank Name</div>
+      <div className="Card-container" style={{ backgroundImage: currentTheme.background }}>
+        <div className="Card-bank-name" style={{color: currentTheme.color}}>
+          {bankName}
+        </div>
         <img src={chip} className="Card-chip"/>
-        <div className="Card-bank-number">1234 5678 9876 5432</div>
+        <div className="Card-bank-number" style={{color: currentTheme.color}}>
+          {cardNumber}
+        </div>
         <div className="Card-bottom-row">
-          <div className="Card-name-holder">Benjamin Johnston</div>
-          <img src={logo} className="Card-logo"/>
+          <div className="Card-name-holder" style={{color: currentTheme.color}}>
+            {cardHolder}
+          </div>
+          {this.renderIssuer()}
         </div>
       </div>
     )
   }
 }
-
-export default Card;
